41365ed05820998e1c4e8d69a3217bced4a74423
zipper <zippp123915@gmail.com>
2020-04-06 16:59:33 +0300

    add header && moblie bg of intro

---
+++ src/scripts/modileMenu.js
@@ -0,0 +1,10 @@
+const toggleButton = document.querySelector('.header__toggle-button');
+const toggleItem = document.querySelector('.header');
+
+toggleItem.classList.toggle('header--close');
+toggleButton.classList.toggle('header__toggle-button--close');
+
+toggleButton.addEventListener('click', event => {
+   toggleItem.classList.toggle('header--close');
+   toggleButton.classList.toggle('header__toggle-button--close');
+});
