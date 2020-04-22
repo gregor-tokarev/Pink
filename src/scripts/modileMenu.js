acce6e6cfac889ab345771c303e78d9ede6f939f
zipper <zippp123915@gmail.com>
2020-04-22 15:38:56 +0300

    Layot of page Photo is completed!

--- src/scripts/modileMenu.js
+++ src/scripts/modileMenu.js
@@ -1,9 +1,12 @@
 const toggleButton = document.querySelector('.header__toggle-button');
 const toggleItem = document.querySelector('.header');
+const intro = document.querySelector('.intro');
 
 toggleItem.classList.toggle('header--close');
 toggleButton.classList.toggle('header__toggle-button--close');
 
+
+intro.addEventListener('click', () => {}, true);
 toggleButton.addEventListener('click', event => {
     console.log('click');
     toggleItem.classList.toggle('header--close');
